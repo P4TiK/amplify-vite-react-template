2c0e01bc9f23195401e4b10350e5f68ce3889a28
Haeun Patrick 'The Star' Kwon <98944680+P4TiK@users.noreply.github.com>
2025-02-26 03:37:46 +0900

    프론트 적용 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
+interface Props {}
+
 
 function App() {
   const { user, signOut } = useAuthenticator();
@@ -19,7 +21,7 @@ function App() {
     client.models.PointRecord.create({ 
       team: 1,
       content: window.prompt("점수 획득 내용을 작성해주세요"),
-      point: Number(window.prompt("점수를 입력해주세요")),
+      point: Number(window.prompt("점수를 입력해주세요(숫자만 입력)")),
      });
   }
   
@@ -27,28 +29,28 @@ function App() {
     client.models.PointRecord.create({ 
       team: 2,
       content: window.prompt("점수 획득 내용을 작성해주세요"),
-      point: Number(window.prompt("점수를 입력해주세요")),
+      point: Number(window.prompt("점수를 입력해주세요(숫자만 입력)")),
      });
   }
   function addPointTeam3() {
     client.models.PointRecord.create({
       team: 3,
       content: window.prompt("점수 획득 내용을 작성해주세요"),
-      point: Number(window.prompt("점수를 입력해주세요")),
+      point: Number(window.prompt("점수를 입력해주세요(숫자만 입력)")),
      });
   }
   function addPointTeam4() {
     client.models.PointRecord.create({
       team: 4,
       content: window.prompt("점수 획득 내용을 작성해주세요"),
-      point: Number(window.prompt("점수를 입력해주세요")),
+      point: Number(window.prompt("점수를 입력해주세요(숫자만 입력)")),
      });
   }
   function addPointTeam5() {
     client.models.PointRecord.create({ 
       team: 5,
       content: window.prompt("점수 획득 내용을 작성해주세요"),
-      point: Number(window.prompt("점수를 입력해주세요")),
+      point: Number(window.prompt("점수를 입력해주세요(숫자만 입력)")),
      });
   }
 
@@ -56,7 +58,7 @@ function App() {
     client.models.PointRecord.create({ 
       team: 6,
       content: window.prompt("점수 획득 내용을 작성해주세요"),
-      point: Number(window.prompt("점수를 입력해주세요")),
+      point: Number(window.prompt("점수를 입력해주세요(숫자만 입력)")),
      });
   }
 
@@ -65,8 +67,162 @@ function App() {
     client.models.PointRecord.delete({ id })
   }
 
+  function Test() {
+	return (
+		<div className="contain">
+			<div className="scroll-view">
+				<span className="text" >
+					{"에브라임 청년부 2025 겨울수련회\n현재 순위를 확인하세요!"}
+				</span>
+				<div className="column">
+					<div className="column2">
+						<img
+							src={"https://storage.googleapis.com/tagjs-prod.appspot.com/bM5l6dsVrS/91cosp6j.png"} 
+							className="image"
+						/>
+						<div className="row-view">
+							<img
+								src={"https://storage.googleapis.com/tagjs-prod.appspot.com/bM5l6dsVrS/qvnam15m.png"} 
+								className="image2"
+							/>
+							<span className="text2" >
+								{"팀이름 1"}
+							</span>
+							<div className="column3">
+								<img
+									src={"https://storage.googleapis.com/tagjs-prod.appspot.com/bM5l6dsVrS/bovj99ps.png"} 
+									className="image3"
+								/>
+								<div className="absolute-view">
+									<span className="text3" >
+										{"0"}
+									</span>
+								</div>
+							</div>
+						</div>
+						<div className="row-view2">
+							<img
+								src={"https://storage.googleapis.com/tagjs-prod.appspot.com/bM5l6dsVrS/rw8ip4wv.png"} 
+								className="image2"
+							/>
+							<span className="text4" >
+								{"팀이름 2"}
+							</span>
+							<div className="column3">
+								<img
+									src={"https://storage.googleapis.com/tagjs-prod.appspot.com/bM5l6dsVrS/dwwmflso.png"} 
+									className="image3"
+								/>
+								<div className="absolute-view">
+									<span className="text3" >
+										{"0"}
+									</span>
+								</div>
+							</div>
+						</div>
+						<div className="row-view3">
+							<img
+								src={"https://storage.googleapis.com/tagjs-prod.appspot.com/bM5l6dsVrS/vxfkmnc8.png"} 
+								className="image4"
+							/>
+							<span className="text4" >
+								{"팀이름 3"}
+							</span>
+							<div className="column4">
+								<img
+									src={"https://storage.googleapis.com/tagjs-prod.appspot.com/bM5l6dsVrS/g1xpm361.png"} 
+									className="image3"
+								/>
+								<div className="absolute-view">
+									<span className="text3" >
+										{"0"}
+									</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<img
+						src={"https://storage.googleapis.com/tagjs-prod.appspot.com/bM5l6dsVrS/onb0yxrp.png"} 
+						className="absolute-image"
+					/>
+					<img
+						src={"https://storage.googleapis.com/tagjs-prod.appspot.com/bM5l6dsVrS/etwgey73.png"} 
+						className="absolute-image2"
+					/>
+				</div>
+				<div className="row-view4">
+					<img
+						src={"https://storage.googleapis.com/tagjs-prod.appspot.com/bM5l6dsVrS/fjijukcq.png"} 
+						className="image2"
+					/>
+					<span className="text4" >
+						{"팀이름 4"}
+					</span>
+					<div className="column5">
+						<img
+							src={"https://storage.googleapis.com/tagjs-prod.appspot.com/bM5l6dsVrS/67szujcm.png"} 
+							className="image3"
+						/>
+						<div className="absolute-view">
+							<span className="text3" >
+								{"0"}
+							</span>
+						</div>
+					</div>
+				</div>
+				<div className="row-view4">
+					<img
+						src={"https://storage.googleapis.com/tagjs-prod.appspot.com/bM5l6dsVrS/hfzea2no.png"} 
+						className="image2"
+					/>
+					<span className="text4" >
+						{"팀이름 5"}
+					</span>
+					<div className="column6">
+						<img
+							src={"https://storage.googleapis.com/tagjs-prod.appspot.com/bM5l6dsVrS/vi8b0kat.png"} 
+							className="image3"
+						/>
+						<div className="absolute-view">
+							<span className="text3" >
+								{"0"}
+							</span>
+						</div>
+					</div>
+				</div>
+				<div className="row-view5">
+					<img
+						src={"https://storage.googleapis.com/tagjs-prod.appspot.com/bM5l6dsVrS/0bb7cf4i.png"} 
+						className="image2"
+					/>
+					<span className="text4" >
+						{"팀이름 6"}
+					</span>
+					<div className="column7">
+						<img
+							src={"https://storage.googleapis.com/tagjs-prod.appspot.com/bM5l6dsVrS/2hvlq3so.png"} 
+							className="image3"
+						/>
+						<div className="absolute-view">
+							<span className="text3" >
+								{"0"}
+							</span>
+						</div>
+					</div>
+				</div>
+				<div className="view">
+					<span className="text5" >
+						{"Copyright © 2025 PATIKLAB."}
+					</span>
+				</div>
+			</div>
+		</div>
+	)
+  }
+
   return (
     <main>
+		<Test />
       <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={addPointTeam1}>TEAM 1 점수 추가</button>
       <button onClick={addPointTeam2}>TEAM 2 점수 추가</button>
